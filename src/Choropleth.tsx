013415a946a0b6a6b1bb380ae06581601d40c28f
Katherine Graessle <katy@focusedlabs.io>
2022-03-08 19:42:18 -0600

    remove boilerplate js files from initial create react app

--- src/Choropleth.tsx
+++ src/Choropleth.tsx
@@ -1,5 +1,66 @@
 import React from "react";
 import {ResponsiveChoropleth} from "@nivo/geo";
+import {FeatureCollection, TestData} from "./us-states"
+import { Toolbar, AppBar, IconButton, Typography } from "@mui/material";
 
-// @ts-ignore
-export const ChoroplethTest: React.FC = () => <ResponsiveChoropleth/>
+
+export const ChoroplethTest: React.FC = () => (
+    <>
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                >
+                </IconButton>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    QUA-FOODIES
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    <ResponsiveChoropleth
+        data={TestData}
+        features={[FeatureCollection.features]}
+        margin={{top: 0, right: 0, bottom: 0, left: 0}}
+        colors="nivo"
+        domain={[0, 1000000]}
+        unknownColor="#666666"
+        label="properties.name"
+        valueFormat=".2s"
+        projectionScale={124}
+        projectionTranslation={[0.5, 0.5]}
+        projectionRotation={[0, 0, 0]}
+        enableGraticule={true}
+        graticuleLineColor="#dddddd"
+        borderWidth={0.5}
+        borderColor="#152538"
+        legends={[
+            {
+                anchor: 'bottom-left',
+                direction: 'column',
+                justify: true,
+                translateX: 20,
+                translateY: -100,
+                itemsSpacing: 0,
+                itemWidth: 94,
+                itemHeight: 18,
+                itemDirection: 'left-to-right',
+                itemTextColor: '#444444',
+                itemOpacity: 0.85,
+                symbolSize: 18,
+                effects: [
+                    {
+                        on: 'hover',
+                        style: {
+                            itemTextColor: '#000000',
+                            itemOpacity: 1
+                        }
+                    }
+                ]
+            }
+        ]}
+    />
+    </>)
