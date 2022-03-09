61b04c51960da59bfdf7df1b429ea0667ae62bee
Katherine Graessle <katy@focusedlabs.io>
2022-03-08 18:11:29 -0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {ChoroplethTest} from "./Choropleth";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ChoroplethTest></ChoroplethTest>
+        </div>
+    );
 }
 
 export default App;
+
+
---
+++ src/Choropleth.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import {ResponsiveChoropleth} from "@nivo/geo";
+
+// @ts-ignore
+export const ChoroplethTest: React.FC = () => <ResponsiveChoropleth/>
