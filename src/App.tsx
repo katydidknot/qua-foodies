0392d268d05c8369242063548bd77691e3d20176
Katherine Graessle <katy@focusedlabs.io>
2022-03-08 20:12:36 -0600

    just initial map

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import {ChoroplethTest} from "./Choropleth";
+import {Choropleth} from "./Choropleth";
 
 function App() {
     return (
         <div className="App">
-            <ChoroplethTest></ChoroplethTest>
+            <Choropleth/>
         </div>
     );
 }
--- src/Choropleth.tsx
+++ src/Choropleth.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import {ResponsiveChoropleth} from "@nivo/geo";
 import {FeatureCollection, TestData} from "./us-states"
-import { Toolbar, AppBar, IconButton, Typography } from "@mui/material";
+import {Toolbar, AppBar, IconButton, Typography, Grid} from "@mui/material";
 
 
-export const ChoroplethTest: React.FC = () => (
+export const Choropleth: React.FC = () => (
     <>
         <AppBar position="static">
             <Toolbar>
@@ -13,54 +13,56 @@ export const ChoroplethTest: React.FC = () => (
                     edge="start"
                     color="inherit"
                     aria-label="menu"
-                    sx={{ mr: 2 }}
+                    sx={{mr: 2}}
                 >
                 </IconButton>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    QUA-FOODIES
+                <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                    qua-foodies
                 </Typography>
             </Toolbar>
         </AppBar>
-    <ResponsiveChoropleth
-        data={TestData}
-        features={[FeatureCollection.features]}
-        margin={{top: 0, right: 0, bottom: 0, left: 0}}
-        colors="nivo"
-        domain={[0, 1000000]}
-        unknownColor="#666666"
-        label="properties.name"
-        valueFormat=".2s"
-        projectionScale={124}
-        projectionTranslation={[0.5, 0.5]}
-        projectionRotation={[0, 0, 0]}
-        enableGraticule={true}
-        graticuleLineColor="#dddddd"
-        borderWidth={0.5}
-        borderColor="#152538"
-        legends={[
-            {
-                anchor: 'bottom-left',
-                direction: 'column',
-                justify: true,
-                translateX: 20,
-                translateY: -100,
-                itemsSpacing: 0,
-                itemWidth: 94,
-                itemHeight: 18,
-                itemDirection: 'left-to-right',
-                itemTextColor: '#444444',
-                itemOpacity: 0.85,
-                symbolSize: 18,
-                effects: [
+        <Grid container justifyContent={"center"} sx={{margin: "2rem", height: "50vh", width: "50vw"}}>
+            <ResponsiveChoropleth
+                data={TestData}
+                features={FeatureCollection.features}
+                margin={{top: 0, right: 0, bottom: 0, left: 20}}
+                colors="nivo"
+                domain={[0, 1000000]}
+                unknownColor="#666666"
+                label="properties.name"
+                valueFormat=".2s"
+                projectionScale={237}
+                projectionTranslation={[1, 1]}
+                projectionRotation={[0, 0, 0]}
+                enableGraticule={true}
+                graticuleLineColor="#dddddd"
+                borderWidth={0.5}
+                borderColor="#152538"
+                legends={[
                     {
-                        on: 'hover',
-                        style: {
-                            itemTextColor: '#000000',
-                            itemOpacity: 1
-                        }
+                        anchor: 'bottom-left',
+                        direction: 'column',
+                        justify: true,
+                        translateX: 0,
+                        translateY: -50,
+                        itemsSpacing: 0,
+                        itemWidth: 94,
+                        itemHeight: 18,
+                        itemDirection: 'left-to-right',
+                        itemTextColor: '#444444',
+                        itemOpacity: 0.85,
+                        symbolSize: 18,
+                        effects: [
+                            {
+                                on: 'hover',
+                                style: {
+                                    itemTextColor: '#000000',
+                                    itemOpacity: 1
+                                }
+                            }
+                        ]
                     }
-                ]
-            }
-        ]}
-    />
+                ]}
+            />
+        </Grid>
     </>)
